35db2c258cd288aec1a76032cae628fe02749b92
Bojan <bojan.cubrilo@kouba.at>
2019-08-03 15:58:55 +0200

    Added passport authentication

---
+++ server/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,5 @@
+const { check, validationResult } = require("express-validator");
+
+module.exports = {
+  register(req, res, next) {}
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,11 +2,14 @@ var express = require("express");
 var router = express.Router();
 var authController = require("../controllers/authenticationController");
 
+var authPolicy = require("../policies/AuthPolicy");
+
 /* GET users listing. */
 router.get("/", function(req, res, next) {
   res.send("respond with a resource");
 });
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+router.get("/logout", authPolicy);
 
 module.exports = router;
