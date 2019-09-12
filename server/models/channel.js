8f8f7e7a74295878aefa10c4d9c500651baf8f4f
Bojan <bojan.cubrilo@kouba.at>
2019-09-12 12:46:40 +0200

    Added message association

--- server/models/channel.js
+++ server/models/channel.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const Channel = sequelize.define("Channel", {}, {});
   Channel.associate = function(models) {
-    Channel.hasMany(models.ChannelMember, { key: "channelId", as: "members" });
+    Channel.hasMany(models.ChannelMember, { key: "channelId", as: "members" }),
+      Channel.hasMany(models.Message, { key: "channelId", as: "messages" });
   };
   return Channel;
 };
