668f07d5b8171d96ac00bf502e53ca47dc9a04f9
Bojan <bojan.cubrilo@kouba.at>
2019-09-21 18:37:38 +0200

    Added listener for new_message

--- client/src/plugins/socket.js
+++ client/src/plugins/socket.js
@@ -2,10 +2,10 @@ import io from "socket.io-client";
 
 export default function socket() {
   return store => {
-    console.log("Connection to server socket");
-    const socket = io("http://localhost:3030");
-    socket.on("userconnected", function(data) {
-      console.log("Connected to the server" + data);
+    const socket = io("http://localhost:3031");
+    socket.on("userconnected", function(data) {});
+    socket.on("new_message", data => {
+      store.commit("chat/addMessage", data);
     });
   };
 }
