f9f5968156401251c379beaa1a0c68b4a9a8ec7d
Bojan <bojan.cubrilo@kouba.at>
2019-09-21 18:36:34 +0200

    Receives socket.io object, calls ChatsController with socket.io

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,26 +3,36 @@ var passport = require("passport");
 var multer = require("multer");
 var router = express.Router();
 
-var UsersController = require("../controllers/UsersController");
-var ChatsController = require("../controllers/ChatsController");
+module.exports = function(io) {
+  var UsersController = require("../controllers/UsersController");
+  var ChatsController = require("../controllers/ChatsController")(io);
 
-var authPolicy = require("../policies/AuthPolicy");
+  var authPolicy = require("../policies/AuthPolicy");
 
-var fileUpload = multer({ dest: "./public/uploads" }).single("file");
+  var fileUpload = multer({ dest: "./public/uploads" }).single("file");
 
-router.get("/users", authPolicy, UsersController.index);
-router.post("/profile", authPolicy, UsersController.saveProfile);
-router.get("/profile/:userId", authPolicy, UsersController.getProfile);
-router.post("/profile/update", authPolicy, UsersController.updateProfile);
-router.post(
-  "/profile/upload-profile-image",
-  [authPolicy, fileUpload],
-  UsersController.uploadProfileImage
-);
+  router.get("/users", authPolicy, UsersController.index);
+  router.post("/profile", authPolicy, UsersController.saveProfile);
+  router.get("/profile/:userId", authPolicy, UsersController.getProfile);
+  router.post("/profile/update", authPolicy, UsersController.updateProfile);
+  router.post(
+    "/profile/upload-profile-image",
+    [authPolicy, fileUpload],
+    UsersController.uploadProfileImage
+  );
 
-router.post("/chat/create-channel", authPolicy, ChatsController.createChannel);
-router.post("/chat/save-message", authPolicy, ChatsController.saveMessage);
-router.post("/chat/delete-message", authPolicy, ChatsController.deleteMessage);
-router.get("/chat/channels", authPolicy, ChatsController.channels);
+  router.post(
+    "/chat/create-channel",
+    authPolicy,
+    ChatsController.createChannel
+  );
+  router.post("/chat/save-message", authPolicy, ChatsController.saveMessage);
+  router.post(
+    "/chat/delete-message",
+    authPolicy,
+    ChatsController.deleteMessage
+  );
+  router.get("/chat/channels", authPolicy, ChatsController.channels);
 
-module.exports = router;
+  return router;
+};
