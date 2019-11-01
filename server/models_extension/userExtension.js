4d6394492da84825228f46056ce7996bb308c121
Bojan <bojan.cubrilo@kouba.at>
2019-11-01 21:52:01 +0100

    Added method getUserPublicProfile

--- server/models_extension/userExtension.js
+++ server/models_extension/userExtension.js
@@ -11,6 +11,48 @@ module.exports = {
     });
     return user;
   },
+  async getUserPublicProfile(username) {
+    var data = {};
+    try {
+      var user = await models.User.findOne({
+        where: {
+          username: username
+        },
+        attributes: ["name", "username"],
+        include: [
+          {
+            model: models.UserProfile,
+            as: "profile",
+            attributes: [
+              "profileImageUrl",
+              "countryId",
+              "languageId",
+              "description",
+              "gender",
+              "interestedInGender"
+            ]
+          }
+        ]
+      });
+      if (user != null)
+        data = {
+          name: user.name,
+          username: user.username,
+          profileImageUrl:
+            user.profile.length > 0 ? user.profile[0].profileImageUrl : "",
+          countryId: user.profile.length > 0 ? user.profile[0].countryId : "",
+          languageId: user.profile.length > 0 ? user.profile[0].languageId : "",
+          description:
+            user.profile.length > 0 ? user.profile[0].description : "",
+          gender: user.profile.length > 0 ? user.profile[0].gender : "",
+          interestedInGender:
+            user.profile.length > 0 ? user.profile[0].interestedInGender : ""
+        };
+    } catch (ex) {
+      console.log(ex);
+    }
+    return data;
+  },
   jwtGetPayload(token) {
     var payload = jwt.verify(token, appConfig.authentication.jwtSecret);
     return payload;
